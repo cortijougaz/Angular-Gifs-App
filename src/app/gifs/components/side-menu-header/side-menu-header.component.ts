e35b8d5aacc4e19fa4c81e1fa9370f18fdf98abe
Ricardo Cortijo <cortijougaz@gmail.com>
2025-10-15 21:24:17 -0500

    GifsApp: Se agregan variables de entorno.

--- src/app/gifs/components/side-menu-header/side-menu-header.component.ts
+++ src/app/gifs/components/side-menu-header/side-menu-header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {environment} from '@environments/environment';
 
 @Component({
   selector: 'gifs-side-menu-header',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './side-menu-header.component.css'
 })
 export class SideMenuHeaderComponent {
-
+  envs = environment;
 }
