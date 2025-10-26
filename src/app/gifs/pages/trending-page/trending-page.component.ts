c60c4acab641a42c10d3c12cf3703cba5c4371c2
Ricardo Cortijo <cortijougaz@gmail.com>
2025-10-26 13:28:40 -0500

    Intermedio-Avanzado: Se agrega la funcionalidad de Scroll.

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -1,6 +1,7 @@
-import {Component, inject, viewChild, ElementRef} from '@angular/core';
+import {Component, inject, viewChild, ElementRef, AfterViewInit} from '@angular/core';
 import {GifListComponent} from '../../components/gif-list/gif-list.component';
 import {GifsService} from '../../services/gifs.service';
+import {ScrollStateService} from '../../../shared/services/scroll-state.service';
 
 @Component({
   selector: 'app-trending-page',
@@ -10,12 +11,33 @@ import {GifsService} from '../../services/gifs.service';
   ],
   styleUrl: './trending-page.component.css'
 })
-export default class TrendingPageComponent {
-  gifService = inject(GifsService);
+export default class TrendingPageComponent implements AfterViewInit{
 
-  scrollDivRef = viewChild<ElementRef>('groupDiv');
+  gifService = inject(GifsService);
+  scrollStateService = inject(ScrollStateService);
+
+  scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv');
+
+  ngAfterViewInit(): void {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if (!scrollDiv) return;
+
+    scrollDiv.scrollTop = this.scrollStateService.trendingScrollState();
+  }
 
   onScroll(event: Event){
     const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if (!scrollDiv) return;
+
+    const scrollTop = scrollDiv.scrollTop;
+    const clientHeight = scrollDiv.clientHeight;
+    const scrollHeight = scrollDiv.scrollHeight;
+
+    const isAtBottom = scrollTop + clientHeight + 300 >= scrollHeight;
+    this.scrollStateService.trendingScrollState.set(scrollTop);
+
+    if (isAtBottom) {
+      this.gifService.loadTrendingGifs();
+    }
   }
 }
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -19,7 +19,8 @@ export class GifsService {
   private http = inject(HttpClient);
 
   trendingGifs = signal<Gif[]>([]);
-  trendingGifsLoading = signal(true);
+  trendingGifsLoading = signal(false);
+  private trendingPage = signal(0);
 
   trendingGifGroup = computed<Gif[][]>(() => {
     const groups = [];
@@ -41,17 +42,21 @@ export class GifsService {
   });
 
   loadTrendingGifs(): void {
+    if (this.trendingGifsLoading()) return;
+    this.trendingGifsLoading.set(true);
+
     this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`, {
       params: {
         api_key: environment.giphyApikey,
         limit: 10,
-        offset: 0,
+        offset: this.trendingPage() * 20,
         //rating: 'g',
       }
     }).subscribe((resp) => {
       const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
-      this.trendingGifs.set(gifs);
+      this.trendingGifs.update( currentGifs => [...currentGifs, ...gifs] );
       this.trendingGifsLoading.set(false);
+      this.trendingPage.update((page) => page + 1);
     });
   }
 
@@ -63,8 +68,7 @@ export class GifsService {
         offset: 0,
         q: query,
         //rating: 'g',
-      }
-    }).pipe(
+      }}).pipe(
       map(({data}) => data),
       map((items) => GifMapper.mapGiphyItemsToGifArray(items)),
       tap(items => {
